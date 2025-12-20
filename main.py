1afb7a8b19a9a76262d197010b9d119ed5747587
tor344 <tor.azgard@icloud.com>
2025-12-20 21:41:06 +0300

    feat: №2 task. add endpoint items

--- main.py
+++ main.py
@@ -9,3 +9,8 @@ def main():
 @app.get("/hello")
 def hello():
     return {"message": "Hello World"}
+
+
+@app.get("/items/{item_id}")
+def items(item_id: int, q: str | None = None):
+    return {"item_id": item_id, "q": q}
