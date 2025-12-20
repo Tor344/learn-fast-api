0aceca18c7030f25193b4edd3ac533c81f51ecf3
tor344 <tor.azgard@icloud.com>
2025-12-20 22:25:01 +0300

    feat: №3 task. Add post endpoint /intems

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 from fastapi import FastAPI
+from pydantic import BaseModel, Field
+
+class Product(BaseModel):
+    name: str
+    price: int = Field(...,ge=0)
+
 
 app = FastAPI()
 
@@ -6,9 +12,15 @@ app = FastAPI()
 def main():
     return {}
 
+
 @app.get("/hello")
 def hello():
     return {"message": "Hello World"}
+
+
+@app.post("/items/")
+def items(product: Product):
+    return {"name": product.name, "price": product.price}
 
 
 @app.get("/items/{item_id}")
